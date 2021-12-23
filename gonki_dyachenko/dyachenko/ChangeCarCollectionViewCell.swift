117d605559c265eb708a7c6184b303a700bf628a
pepellsd <denlox16@list.ru>
2021-12-23 03:37:23 +0300

    gonki and calculator

---
+++ gonki_dyachenko/dyachenko/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  Gonki
+//
+//  Created by user on 18.10.2021.
+//
+
+import UIKit
+
+class ChangeCarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carPicture: UIImageView!
+
+    
+}
